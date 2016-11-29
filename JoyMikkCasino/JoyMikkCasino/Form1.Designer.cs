d228d94a09fb499654637b9e150f4fa7bf9052c5
BogdanMikk <skrynnyk.b@gmail.com>
2016-11-29 22:55:56 +0200

    Add files via upload

---
+++ JoyMikkCasino/JoyMikkCasino/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace JoyMikkCasino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbcash = new System.Windows.Forms.Label();
+            this.dvig1 = new System.Windows.Forms.Timer(this.components);
+            this.dvig2 = new System.Windows.Forms.Timer(this.components);
+            this.dvig3 = new System.Windows.Forms.Timer(this.components);
+            this.stop1 = new System.Windows.Forms.Timer(this.components);
+            this.stop2 = new System.Windows.Forms.Timer(this.components);
+            this.stop3 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.lbcash);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 357);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(524, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Инормация";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 75);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkOrange;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(112, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 110);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkOrange;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(219, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 110);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DarkOrange;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(326, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 110);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(126, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Ваши деньги:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(126, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Сумма ставки:";
+            // 
+            // lbcash
+            // 
+            this.lbcash.AutoSize = true;
+            this.lbcash.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbcash.Location = new System.Drawing.Point(218, 31);
+            this.lbcash.Name = "lbcash";
+            this.lbcash.Size = new System.Drawing.Size(32, 16);
+            this.lbcash.TabIndex = 2;
+            this.lbcash.Text = "100";
+            // 
+            // dvig1
+            // 
+            this.dvig1.Interval = 10;
+            this.dvig1.Tick += new System.EventHandler(this.dvig1_Tick);
+            // 
+            // dvig2
+            // 
+            this.dvig2.Interval = 10;
+            this.dvig2.Tick += new System.EventHandler(this.dvig2_Tick);
+            // 
+            // dvig3
+            // 
+            this.dvig3.Interval = 10;
+            this.dvig3.Tick += new System.EventHandler(this.dvig3_Tick);
+            // 
+            // stop1
+            // 
+            this.stop1.Interval = 1500;
+            this.stop1.Tick += new System.EventHandler(this.stop1_Tick);
+            // 
+            // stop2
+            // 
+            this.stop2.Interval = 2500;
+            this.stop2.Tick += new System.EventHandler(this.stop2_Tick);
+            // 
+            // stop3
+            // 
+            this.stop3.Interval = 3000;
+            this.stop3.Tick += new System.EventHandler(this.stop3_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(218, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 16);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "10$";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 469);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbcash;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer dvig1;
+        private System.Windows.Forms.Timer dvig2;
+        private System.Windows.Forms.Timer dvig3;
+        private System.Windows.Forms.Timer stop1;
+        private System.Windows.Forms.Timer stop2;
+        private System.Windows.Forms.Timer stop3;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ JoyMikkCasino/JoyMikkCasino/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JoyMikkCasino
+{
+    public partial class Form1 : Form
+    {
+        int cash = 100;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dvig1_Tick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int dig = rand.Next(8);
+            label1.Text = System.Convert.ToString(dig);
+        }
+
+        private void dvig2_Tick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int dig = rand.Next(8);
+            label2.Text = System.Convert.ToString(dig);
+        }
+
+        private void dvig3_Tick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int dig = rand.Next(8);
+            label3.Text = System.Convert.ToString(dig);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbcash.Text = System.Convert.ToString(cash);
+            dvig1.Enabled = true;
+            dvig2.Enabled = true;
+            dvig3.Enabled = true;
+            stop1.Enabled = true;
+            stop2.Enabled = true;
+            stop3.Enabled = true;
+        }
+
+        private void stop1_Tick(object sender, EventArgs e)
+        {
+            dvig1.Enabled = false;
+            stop1.Enabled = false;
+        }
+
+        private void stop2_Tick(object sender, EventArgs e)
+        {
+            dvig2.Enabled = false;
+            stop2.Enabled = false;
+        }
+
+        private void stop3_Tick(object sender, EventArgs e)
+        {
+            dvig3.Enabled = false;
+            stop3.Enabled = false;
+            // ***********************
+            // Swich case 80 lvl
+            //************************
+            //WINS
+            if (cash <= 0)
+            {
+                MessageBox.Show("Вы все проиграли!");
+                button1.Enabled = false;
+            }
+            if ((label1.Text == "2") && (label2.Text == "2") && (label3.Text == "8"))
+            {
+                MessageBox.Show("Вы выиграли супер бонус от МИХИ228 228$");
+                cash += 228;
+            }
+
+            if ((label1.Text == "2") && (label2.Text == "2") && (label3.Text == "2"))
+            {
+                MessageBox.Show("Вы выиграли 100$");
+                cash += 100;
+            }
+
+            if ((label1.Text == "1") && (label2.Text == "1") && (label3.Text == "1"))
+            {
+                MessageBox.Show("Вы выиграли 100$");
+                cash += 100;
+            }
+
+            if ((label1.Text == "3") && (label2.Text == "3") && (label3.Text == "3"))
+            {
+                MessageBox.Show("Вы выиграли 100$");
+                cash += 100;
+            }
+
+            if ((label1.Text == "4") && (label2.Text == "4") && (label3.Text == "4"))
+            {
+                MessageBox.Show("Вы выиграли 100$");
+                cash += 100;
+            }
+
+            if ((label1.Text == "5") && (label2.Text == "5") && (label3.Text == "5"))
+            {
+                MessageBox.Show("Вы выиграли 100$");
+                cash += 100;
+            }
+
+            if ((label1.Text == "6") && (label2.Text == "6") && (label3.Text == "6"))
+            {
+                MessageBox.Show("Вы выиграли 100$");
+                cash += 100;
+            }
+
+            if ((label1.Text == "7") && (label2.Text == "7") && (label3.Text == "7"))
+            {
+                MessageBox.Show("Вы выиграли 100$");
+                cash += 100;
+            }
+
+            if ((label1.Text == "3") && (label2.Text == "2") && (label3.Text == "2"))
+            {
+                MessageBox.Show("Вы выиграли супер бонус от СОЛО322 322$");
+                cash += 322;
+            }
+            if ((label1.Text == "7") || (label2.Text == "7") || (label3.Text == "7"))
+            {
+                MessageBox.Show("Вы выиграли 10$");
+                cash += 10;
+            }
+                //LOSE
+            else
+            {
+                cash -= 10;
+            }
+            lbcash.Text = System.Convert.ToString(cash);
+        }
+    }
+}
